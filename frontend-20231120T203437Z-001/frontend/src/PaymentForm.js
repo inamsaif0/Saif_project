23807e4c23cdfe99574e99fd27608994cf1b0279
Inam Ullah Yousufzai <58142833+inamsaif0@users.noreply.github.com>
2023-11-28 23:00:14 +0500

    Update PaymentForm.js
    sdsds

--- frontend-20231120T203437Z-001/frontend/src/PaymentForm.js
+++ frontend-20231120T203437Z-001/frontend/src/PaymentForm.js
@@ -42,7 +42,7 @@ function CheckboxPage() {
   const handleContinue = () => {
     if (isAnyCheckboxSelected()) {
       axios
-        .post('http://localhost:8081/step3', { step3 })
+        .post('http://45.55.45.170:8081/step3', { step3 })
         .catch((error) => {
           console.error('API error:', error);
         });
